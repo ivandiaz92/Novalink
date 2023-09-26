a16bb2cfa744784b426fd13095b93a640c2ad736
idiaz <i.diaz@ukko.mx>
2023-09-26 03:46:50 -0600

    unccoment else in php

--- dist/contactform.php
+++ dist/contactform.php
@@ -33,8 +33,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //     } else {
 //         echo "Oops! Something went wrong. Please try again later.";
 //     }
-// } else {
-//     // Handle the case when the form is not submitted
-//     echo "Form submission error.";
-// }
+} else {
+    // Handle the case when the form is not submitted
+    echo "Form submission error.";
+}
 ?>
